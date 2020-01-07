06706722a1ead14b31654960cdb1c1602790c713
合田和樹 <goda@eyemovic.com>
2020-01-07 18:20:55 +0900

    ひとりごとの判定以外を実装

--- web/public/index.php
+++ web/public/index.php
@@ -6,6 +6,25 @@ $connection = new TwistOAuth($consumer_key, $consumer_secret, $access_token, $ac
 
 // 自分のタイムラインを取得
 $home_params = ['count' => '10'];
-$home        = $connection->get('statuses/home_timeline', $home_params);
+$tweets      = $connection->get('statuses/home_timeline', $home_params);
 
-var_dump($home);
+foreach ($tweets as $tweet)
+{
+	$image = $tweet->user->profile_image_url;
+	echo "<img src={$image}>";
+
+	$text = $tweet->text;
+	echo "<div>{$text}</div>";
+
+	$isAlone = ($tweet->in_reply_to_user_id_str === NULL) ? TRUE : FALSE;
+
+	$tweet_id = $tweet->id_str;
+
+	// メンションが無い場合のみファボする。
+	if ($isAlone)
+	{
+		$params = ["id" => $tweet_id];
+		// $favorite = $connection->post("favorites/create.json", $params);
+		echo "<p>ひとりごと</p>";
+	}
+}
