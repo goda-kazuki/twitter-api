6612506f4dd387456d3f778b6a36593b17775910
合田和樹 <goda@eyemovic.com>
2020-01-09 10:00:26 +0900

    微調整

--- web/public/index.php
+++ web/public/index.php
@@ -5,15 +5,17 @@ require "config.php";
 $connection = new TwistOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
 
 // 自分のタイムラインを取得
-$home_params = ['count' => '10'];
+$home_params = ['count' => '5'];
 $tweets      = $connection->get('statuses/home_timeline', $home_params);
 
 foreach ($tweets as $tweet)
 {
 	$isAlone = is_null($tweet->in_reply_to_user_id_str);
 
-	// リプライではない かつ　リツイートでない場合
-	if ($isAlone && ! isset($tweet->retweeted_status))
+	// リプライではない かつ　リツイートでない場合　かつ　未お気に入り
+	if ($isAlone &&
+	    ! isset($tweet->retweeted_status) &&
+	    ! $tweet->favorited)
 	{
 		$params   = ["id" => $tweet->id_str];
 		$favorite = $connection->post("favorites/create.json", $params);
